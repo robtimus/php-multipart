4854f4f9f3404434d468593706d47f23ebea8d01
robtimus <robtimus@users.noreply.github.com>
2023-01-16 19:09:48 +0000

    Updated for version 1.0.1

--- apidocs/elementlist.js
+++ apidocs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Robtimus\\Multipart\\Multipart"],["c","Robtimus\\Multipart\\MultipartAlternative"],["c","Robtimus\\Multipart\\MultipartFormData"],["c","Robtimus\\Multipart\\MultipartMixed"],["c","Robtimus\\Multipart\\MultipartRelated"],["c","Robtimus\\Multipart\\Util"]];
+ApiGen.elements = [["c","Robtimus\\Multipart\\Multipart"],["c","Robtimus\\Multipart\\MultipartAlternative"],["c","Robtimus\\Multipart\\MultipartFormData"],["c","Robtimus\\Multipart\\MultipartMixed"],["c","Robtimus\\Multipart\\MultipartRelated"]];
