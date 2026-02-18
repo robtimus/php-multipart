38042f473670426e7cf957b8047fcd10d179ed63
Rob Spoor <rob@icemanx.nl>
2026-02-18 18:08:47 +0100

    Use random_bytes instead of mt_rand for generating boundaries

--- src/Robtimus/Multipart/Multipart.php
+++ src/Robtimus/Multipart/Multipart.php
@@ -96,17 +96,10 @@ abstract class Multipart
      */
     private function _generateBoundary(): string
     {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     /**
